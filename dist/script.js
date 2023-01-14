ea9f2a0737913deeb5698d9a1900d632eebe59f4
ATEOTG <bryansantos9797@gmail.com>
2023-01-13 20:59:09 -0500

    added mobile dropdowm menu:

---
+++ dist/script.js
@@ -0,0 +1,12 @@
+const mobileNav = document.querySelector(".nav-menu__hamburger");
+const mobileDropdownMenu = document.querySelector(".nav-menu__mobile-list");
+
+mobileNav.addEventListener("click", function () {
+  mobileNav.classList.toggle("clicked");
+  mobileDropdownMenu.classList.toggle("clicked");
+  if (mobileNav.classList.contains("clicked")) {
+    mobileNav.src = "./images/icon-close.svg";
+  } else {
+    mobileNav.src = "./images/icon-hamburger.svg";
+  }
+});
