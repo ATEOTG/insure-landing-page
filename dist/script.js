ed375515624610e0300c128c6ab608c80fa20d3f
ATEOTG <bryansantos9797@gmail.com>
2023-01-15 20:35:12 -0500

    added responsive design

--- dist/script.js
+++ dist/script.js
@@ -1,12 +1,14 @@
 const mobileNav = document.querySelector(".nav-menu__hamburger");
 const mobileDropdownMenu = document.querySelector(".nav-menu__mobile-list");
 
-mobileNav.addEventListener("click", function () {
-  mobileNav.classList.toggle("clicked");
-  mobileDropdownMenu.classList.toggle("clicked");
-  if (mobileNav.classList.contains("clicked")) {
-    mobileNav.src = "./images/icon-close.svg";
-  } else {
-    mobileNav.src = "./images/icon-hamburger.svg";
-  }
+[mobileNav, mobileDropdownMenu].forEach((el) => {
+  el.addEventListener("click", function () {
+    mobileNav.classList.toggle("clicked");
+    mobileDropdownMenu.classList.toggle("clicked");
+    if (mobileNav.classList.contains("clicked")) {
+      mobileNav.src = "./images/icon-close.svg";
+    } else {
+      mobileNav.src = "./images/icon-hamburger.svg";
+    }
+  });
 });
