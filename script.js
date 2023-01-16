e2f9a7747c3ac6b531f1c2801ba09ae3e751006d
ATEOTG <bryansantos9797@gmail.com>
2023-01-15 21:03:06 -0500

    moved files

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const mobileDropdownMenu = document.querySelector(".nav-menu__mobile-list");
     mobileNav.classList.toggle("clicked");
     mobileDropdownMenu.classList.toggle("clicked");
     if (mobileNav.classList.contains("clicked")) {
-      mobileNav.src = "./images/icon-close.svg";
+      mobileNav.src = "./dist/images/icon-close.svg";
     } else {
-      mobileNav.src = "./images/icon-hamburger.svg";
+      mobileNav.src = "./dist/images/icon-hamburger.svg";
     }
   });
 });
